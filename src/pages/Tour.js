e2f768da7b5d304a8af6b45196bd02652b6f8f7f
kortmiranda <kortneymiranda@outlook.com>
2022-05-17 18:07:41 -0500

    add accordian and footer modal

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,3 +1,47 @@
-const Tour = ()=> <div>Hello</div>;
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box'
+import ImageCollage from '../components/ImageCollage';
+import SimpleAccordion from '../components/Accordian';
+import Paper from '@mui/material/Paper'
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BasicModal from '../components/Modal'
 
+const Tour = () => {
+    return (
+    <Container sx={{width: 900}}>
+        <Typography variant="h3" component="h1" marginTop={3}>
+            Explore the World in Vegas
+        </Typography>
+        <Box marginTop={3} sx={{display: "flex"}}>
+            <img 
+                src="http://res.cloudinary.com/simpleview/image/upload/v1497480003/clients/lasvegas/strip_b86ddbea-3add-4995-b449-ac85d700b027.jpg" 
+                alt="Las Vegas" 
+                height={325}
+                width={525} />
+            <ImageCollage />
+        </Box>
+        <Box>
+            <Typography variant="h6" component="h4" marginTop={3}>
+                About this ticket
+            </Typography>
+            <Typography variant="paragraph" component="p" marginTop={3}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography>
+        </Box>
+        <Box marginBottom={10}>
+            <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+                Frequently Asked Questions
+            </Typography>
+            <SimpleAccordion />
+        </Box>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation>
+          <BasicModal />
+        </BottomNavigation>
+      </Paper>
+    </Container>
+    
+    )
+}
 export default Tour;
