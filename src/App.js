71cd4693e424bea7722a7d77e5059f6001c9c172
kortmiranda <kortneymiranda@outlook.com>
2022-05-17 14:58:50 -0500

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import './App.css';
-import TourCard from './components/TourCard';
-import Container from '@mui/material/Container';
-import Grid from "@mui/material/Grid";
-import SearchAppBar from './components/AppBar';
-import cities from './data.json'
-import { Typography } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SearchAppBar from './components/AppBar'
+import Tour from './pages/Tour'
+
 
 function App() {
   return (
-    <div className="App">
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-         <>
-          <Typography 
-            variant="h4"
-            componenet="h2"
-            marginTop={5}
-            marginBottom={3}
-          >
-           Top {city.name} Tours 
-          </Typography>
-          <Grid container spacing={5}>
-          {city.tours.map((tour, index) => <TourCard tour={tour} key={index}/> )}
-        </Grid>
-         </>
-        ))}
-        
-      </Container>
-    </div>
-  );
+  <BrowserRouter>
+  <SearchAppBar />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/:id" element={<Tour />}/>
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
